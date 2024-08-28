c198e1227a369d6a67d60e3e562b57220341bb1c
Fadliabdill <fadliabdill287@gmail.com>
2024-08-28 21:35:09 +0700

    tes

--- resources/views/admin/DAP-DAR/index.blade.php
+++ resources/views/admin/DAP-DAR/index.blade.php
@@ -22,7 +22,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ url('/home') }}">home</a></li>
-                        <li class="breadcrumb-item active">DAP/DAR</li>
+                        <li class="breadcrumb-item active">DAP-DAR</li>
                     </ol>
                 </div>
             </div>
